fc4201bf920b81d1516d3ae522c99cd72649df5f
Raphael Barreto <tnt.raphael@gmail.com>
2019-09-30 13:22:39 -0300

    Flutter Tutorial for Beginners #28 - WorldTime Custom Class

---
+++ lib/services/world_time.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+
+  String location; // location name for the UI
+  String time; // the time of the location
+  String flag; // url to an asset flag icon
+  String url; // location url api endpoint
+
+  WorldTime({ this.location, this.flag, this.url});
+
+  Future<void> getTime() async {
+    // make the request
+    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+    Map data = jsonDecode(response.body);
+    //print(data);
+
+    // get properties from json
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(0,3);
+    //print(datetime);
+    //print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    // set the time property
+    time = now.toString();
+
+  }
+
+}
